2aeaa6db7e31d2318e26aae3f7ad2d98fb40f521
Ivan Membo <ivotyz@gmail.com>
2025-09-21 05:59:06 +0300

    Add function with urls and templates

--- django-models/LibraryProject/bookshelf/delete.md
+++ django-models/LibraryProject/bookshelf/delete.md
@@ -1,4 +1,6 @@
-﻿book.delete()
+﻿from bookshelf.models import Book
+
+book.delete()
 # (1, {'bookshelf.Book': 1})
 
 Book.objects.all()
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.views.generic import DetailView
+from .models import Book, Library
 
-# Create your views here.
+# 📌 Function-based view: List all books
+def list_books(request):
+    books = Book.objects.select_related('author').all()
+    # Render an HTML template
+    return render(request, 'list_books.html', {'books': books})
+
+
+# 📌 Class-based view: Display details for a specific library
+class LibraryDetailView(DetailView):
+    model = Library
+    template_name = 'library_detail.html'  # template to use
+    context_object_name = 'library'        # context name in template
+
+    
