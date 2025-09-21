885c3617c241cb973476b49b758a8be93eef8c28
Ivan Membo <ivotyz@gmail.com>
2025-09-21 10:45:54 +0300

    add

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -1,23 +1,12 @@
 from django.contrib import admin
-
-# Register your models here.
-from django.contrib import admin
-from .models import Book
+from .models import Book, Author
 
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
-    # Fields to display in the list view
-    list_display = ("title", "author", "publication_year")
+    list_display = ('title', 'author',)
+    #list_filter = ('publication_year',)
+    #ordering = ('publication_year',)
 
-    # Add filters on the right side of the admin page
-    list_filter = ("publication_year", "author")
-
-    # Enable search functionality (search by title and author)
-    search_fields = ("title", "author")
-
-    # Optional: ordering of books in admin
-    ordering = ("publication_year", "title")
-
-
-
-admin.site.register
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('name',)
--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -1,13 +1,15 @@
 from django.db import models
 
-# Create your models here.
-class Book(models.Model):
-    title = models.CharField(max_length=200)
-    author = models.CharField(max_length=100)
-    publication_year = models.IntegerField()
-
+class Author(models.Model):
+    name = models.CharField(max_length=100)
 
     def __str__(self):
-        return f"{self.title} by {self.author} ({self.publication_year})"
-        
+        return self.name
 
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)  # now it works
+    # other fields ...
+
+    def __str__(self):
+        return self.title
